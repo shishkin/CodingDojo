c3357a66c5c50c65be0fdebdc737187698331d31
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-11-18 21:11:11 +0100

    first failing test

--- KataTennis/Tests.cs
+++ KataTennis/Tests.cs
@@ -9,5 +9,25 @@ namespace KataTennis
     [TestClass]
     public class Tests
     {
+        [TestMethod]
+        public void Given_0_0_when_left_scores_should_state_1_0()
+        {
+            var game = new Game()
+                .ScoreLeft();
+            Assert.AreEqual(1, game.LeftScore);
+            Assert.AreEqual(0, game.RightScore);
+        }
+    }
+
+    public class Game
+    {
+        public int LeftScore { get; private set; }
+        
+        public int RightScore { get; private set; }
+
+        public Game ScoreLeft()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
