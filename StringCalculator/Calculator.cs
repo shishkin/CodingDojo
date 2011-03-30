932516e6eb4069b697bc782bc2bb0ad3ab85977e
shishkin <sergei.shishkin@gmail.com>
2011-03-30 10:40:58 +0200

    first failing test

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace StringCalculator
+{
+    using System;
+
+    using Xunit;
+    using Should;
+
+    public class Tests
+    {
+        [Fact]
+        public void Evaluate_decimal_constant()
+        {
+            new Calculator().Evaluate("3,14").ShouldEqual(3.14m);
+        }
+    }
+
+    public class Calculator
+    {
+        public decimal Evaluate(string expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
