937615e66913de3e9d0f27e8a41dcf5f1d722ddb
shishkin <sergei.shishkin@gmail.com>
2011-03-30 13:16:47 +0200

    multiplication

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -12,6 +12,7 @@
         [InlineData("Evaluate constant", "3,14", 3.14)]
         [InlineData("Evaluate simple addition", "3+4", 7)]
         [InlineData("Ignore whitespaces", " 2,5 + \n 6\t", 8.5)]
+        [InlineData("Multiplication", "6 * 1,5", 9)]
         public void Examples(
             string scenario,
             string expression,
@@ -26,6 +27,19 @@
     public class Calculator
     {
         public double Evaluate(string expression)
+        {
+            return EvaluateMultiplications(expression);
+        }
+
+        private double EvaluateMultiplications(string expression)
+        {
+            return expression
+                .Split('*')
+                .Select(EvaluateAdditions)
+                .Aggregate((a, b) => a * b);
+        }
+
+        private double EvaluateAdditions(string expression)
         {
             return expression
                 .Split('+')
