a22bd018f8d0e30e21215fa066f6bf1a21d9fa1a
shishkin <sergei.shishkin@gmail.com>
2011-03-30 13:03:05 +0200

    ignore white spaces

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -11,6 +11,7 @@
         [Theory]
         [InlineData("Evaluate constant", "3,14", 3.14)]
         [InlineData("Evaluate simple addition", "3+4", 7)]
+        [InlineData("Ignore whitespaces", " 2,5 + \n 6\t", 8.5)]
         public void Examples(
             string scenario,
             string expression,
