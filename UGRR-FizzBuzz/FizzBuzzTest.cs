8d1e4fd6003958bbfbafe349087645aba8fa57e3
shishkin <sergei.shishkin@gmail.com>
2011-04-27 19:53:36 +0200

    Durch 3 ist grün

--- UGRR-FizzBuzz/FizzBuzzTest.cs
+++ UGRR-FizzBuzz/FizzBuzzTest.cs
@@ -9,6 +9,7 @@
         [Theory]
         [InlineData(1, "1", "One is one")]
         [InlineData(2, "2", "Two is two")]
+        [InlineData(3, "Fizz", "Three is Fizz")]
         public void NumberIsNumber(
             int number,
             string result,
@@ -24,6 +25,10 @@
     {
         public string Test(int number)
         {
+            if (number == 3)
+            {
+                return "Fizz";
+            }
             return number.ToString();
         }
     }
