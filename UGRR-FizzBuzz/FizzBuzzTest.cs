a39029a0fbd3f364cfa2b32ccf0ec78362e74d4b
shishkin <sergei.shishkin@gmail.com>
2011-04-27 19:23:01 +0200

    Begin Fizz-Buzz; First test green

---
+++ UGRR-FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,27 @@
+﻿namespace UGRR_FizzBuzz
+{
+    using System;
+    using System.Linq;
+
+    using Should;
+
+    using Xunit;
+
+    public class FizzBuzzTest
+    {
+        [Fact]
+        public void OneIsOne()
+        {
+            new FizzBuzz().Test(1)
+                .ShouldEqual("1");
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public string Test(int number)
+        {
+            return "1";
+        }
+    }
+}
