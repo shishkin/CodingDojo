79029e3c38b4cd120b58c1839d3a95825e3b2ec1
shishkin <sergei.shishkin@gmail.com>
2011-04-27 19:37:29 +0200

    Second test green

--- UGRR-FizzBuzz/FizzBuzzTest.cs
+++ UGRR-FizzBuzz/FizzBuzzTest.cs
@@ -12,8 +12,17 @@
         [Fact]
         public void OneIsOne()
         {
-            new FizzBuzz().Test(1)
+            new FizzBuzz()
+                .Test(1)
                 .ShouldEqual("1");
+        }
+
+        [Fact]
+        public void TwoIsTwo()
+        {
+            new FizzBuzz()
+                .Test(2)
+                .ShouldEqual("2");
         }
     }
 
@@ -21,7 +30,7 @@
     {
         public string Test(int number)
         {
-            return "1";
+            return number.ToString();
         }
     }
 }
