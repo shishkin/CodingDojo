162e758300935d8255657bd511aa3f4fca8ba538
shishkin <sergei.shishkin@gmail.com>
2011-04-27 20:02:07 +0200

    Implemented 4 and 5

--- UGRR-FizzBuzz/FizzBuzzTest.cs
+++ UGRR-FizzBuzz/FizzBuzzTest.cs
@@ -10,6 +10,8 @@
         [InlineData(1, "1", "One is one")]
         [InlineData(2, "2", "Two is two")]
         [InlineData(3, "Fizz", "Three is Fizz")]
+        [InlineData(4, "4", "Four is Four")]
+        [InlineData(5, "Buzz", "Five is Buzz")]
         public void NumberIsNumber(
             int number,
             string result,
@@ -26,8 +28,8 @@
         public string Test(int number)
         {
             return number == 3 ?
-                "Fizz" :
-                number.ToString();
+                "Fizz" + (number == 5 ? "Buzz" : ""):
+                number == 5 ? "Buzz" : number.ToString();
         }
     }
 }
